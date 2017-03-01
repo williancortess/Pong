dd19cde4d2036b93a84bddddc2e7d25e094d173c
williancortess <williankzn@gmail.com>
2017-02-28 23:01:42 -0300

    Tilesets

--- app/src/main/java/com/willian/cortes/pong/GameView.java
+++ app/src/main/java/com/willian/cortes/pong/GameView.java
@@ -3,12 +3,15 @@ package com.willian.cortes.pong;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.graphics.Rect;
 
 import com.willian.cortes.simplegameenginev1.SGEntity;
 import com.willian.cortes.simplegameenginev1.SGImage;
 import com.willian.cortes.simplegameenginev1.SGImageFactory;
 import com.willian.cortes.simplegameenginev1.SGRenderer;
+import com.willian.cortes.simplegameenginev1.SGTileset;
 import com.willian.cortes.simplegameenginev1.SGView;
 
 import java.util.ArrayList;
@@ -19,41 +22,60 @@ import java.util.ArrayList;
 
 public class GameView extends SGView {
 
-    private boolean 		mIsDebug = false;
-    private GameModel 	    mModel;
-    private Rect            mTempSrcRect = new Rect();
-    private SGImage         mBallImage;
-    private SGImage 		mOpponentImage;
-    private SGImage 		mPlayerImage;
+    private boolean mIsDebug = false;
+    private GameModel mModel;
+    private Rect mTempSrcRect = new Rect();
+//    private SGImage         mBallImage;
+//    private SGImage 		mOpponentImage;
+//    private SGImage 		mPlayerImage;
 
-    private GameView(Context context)
-    {
+    private SGTileset mTsetBall;
+    private SGTileset mTsetOpponent;
+    private SGTileset mTsetPlayer;
+
+    private GameView(Context context) {
         super(context);
     }
 
-    public GameView(Context context, GameModel model)
-    {
+    public GameView(Context context, GameModel model) {
         super(context);
         mModel = model;
     }
 
     @Override
-    public void setup()
-    {
+    public void setup() {
         mModel.setup(); //Invoca o setup da classe GameModel
 
         SGImageFactory imageFactory = getImageFactory();
 
-        mBallImage = imageFactory.createImage("ball.png");
-        mPlayerImage = imageFactory.createImage("player.png");
-        mOpponentImage = imageFactory.createImage("opponent.png");
+        // Descrição da bola
+        SGImage ballImage = imageFactory.createImage("ball.png");
+
+        //4 x 2 = 8 tiles a bola -  Calcula as dimensoes de tile
+        mTsetBall = new SGTileset(ballImage, new Point(4, 2), null);
+
+        // Paddle do oponente
+        SGImage opponentImage = imageFactory.createImage("opponent.png");
+        //8 x 2 = 16 tiles o paddle
+        mTsetOpponent = new SGTileset(opponentImage, new Point(8, 2),
+                new Rect(0, 0,
+                        GameModel.PADDLE_WIDTH,
+                        GameModel.PADDLE_HEIGHT));
+        //Rect indca a area dentro de um tile que sera realmente desenhada
+
+        // Paddle do jogador
+        SGImage playerImage = imageFactory.createImage("player.png");
+        mTsetPlayer = new SGTileset(playerImage, new Point(8, 2),
+                new Rect(0, 0,
+                        GameModel.PADDLE_WIDTH,
+                        GameModel.PADDLE_HEIGHT));
     }
 
     @Override
-    public void step(Canvas canvas, float elapsedTimeInSeconds)
-    {
+    public void step(Canvas canvas, float elapsedTimeInSeconds) {
         //Chama o metodo steep do modelo dentro da visao para sincronizar o modelo com o loop do jogo
         mModel.step(elapsedTimeInSeconds);
+
         SGRenderer renderer = getRenderer();
         renderer.beginDrawing(canvas, Color.BLACK);
 
@@ -80,33 +102,34 @@ public class GameView extends SGView {
             {
                 if(currentEntity.getCategory() != "trigger")
                 {
+                    SGTileset tileset;
+
                     if(currentEntity.getId() == GameModel.PLAYER_ID)
                     {
-                        mTempSrcRect.set(0, 0, GameModel.PADDLE_WIDTH, GameModel.PADDLE_HEIGHT);
-                        renderer.drawImage(mPlayerImage, mTempSrcRect,
-                                currentEntity.getPosition(),
-                                currentEntity.getDimensions());
+                        tileset = mTsetPlayer;
                     }
                     else if(currentEntity.getId() == GameModel.OPPONENT_ID)
                     {
-                        mTempSrcRect.set(0, 0, GameModel.PADDLE_WIDTH, GameModel.PADDLE_HEIGHT);
-                        renderer.drawImage(mOpponentImage, mTempSrcRect,
-                                currentEntity.getPosition(),
-                                currentEntity.getDimensions());
+                        tileset = mTsetOpponent;
                     }
                     else // (currentEntity.getId() == GameModel.BALL_ID)
                     {
-                        mTempSrcRect.set(0, 0, GameModel.BALL_SIZE, GameModel.BALL_SIZE);
-                        renderer.drawImage(mBallImage, mTempSrcRect,
-                                currentEntity.getPosition(),
-                                currentEntity.getDimensions());
+                        tileset = mTsetBall;
                     }
+
+                    //Posicao e dimensao da entidade
+                    PointF position = currentEntity.getPosition();
+                    PointF dimensions = currentEntity.getDimensions();
+                    Rect drawingArea = tileset.getTile(0); //Area de desenho
+
+                    renderer.drawImage(tileset.getImage(), drawingArea, position, dimensions);
                 }
             }
         }
+
         renderer.endDrawing();
     }
-    }
+}
 
 //    protected void setup()
 //    {
